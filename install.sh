b529f97e409f78cd2266462d00a9e44adbfb0c85
Dylan Starink <dylan@starink.com>
2022-10-01 17:36:44 -0700

    Require running script as root

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 #################
 # Let's install some prereqs for our BeagleBone.
 # Are you ready?
@@ -9,15 +14,15 @@
 # Set up a locale which supports UTF-8 encoding
 ###
 
-sudo apt-get update && sudo apt-get upgrade
+apt-get update && sudo apt-get upgrade
 
 ###
 # Install Python
 ###
-sudo apt-get update
-sudo apt-get install build-essential python3-dev python3-pip -y
+apt-get update
+apt-get install build-essential python3-dev python3-pip -y
 
 ###
 # Lets install all of our Python Libraries
 ###
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
