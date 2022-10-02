7f88874be3eb54a152c4cbb92787ce2068aa2c35
Dylan Starink <dylan@starink.com>
2022-10-01 18:50:36 -0700

    Not needed for now, since we are not using SPI

--- boat/sensors/readInputs.py
+++
@@ -1,7 +0,0 @@
-import Adafruit_BBIO
-# spi = Adafruit_BBIO.SPI(bus, device) #/dev/spidev<bus>.<device>
-
-# /dev/spidev0.0
-spi = Adafruit_BBIO.SPI(0, 0)
-print(spi.xfer2([32, 11, 110, 22, 220]))
-spi.close()
