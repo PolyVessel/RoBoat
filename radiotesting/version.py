bbd02f5876341e60625308d50f36df8498066967
waldenhillegass <waldenhillegass@gmail.com>
2022-10-26 13:31:20 -0700

    ee

---
+++ radiotesting/version.py
@@ -0,0 +1,11 @@
+from pip import main
+import serial 
+
+
+def main():
+   ser = serial.Serial('/dev/tty0', baudrate=9600, timeout=1, stopbits=serial.STOPBITS_ONE)
+   ser.write(0xC3C3C3)
+   print(hex(ser.read(8)))
+
+if __name__ == "__main__":
+   main()
