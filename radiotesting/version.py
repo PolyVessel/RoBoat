267f65d98673b19da5e584ab41d59b4b54d5d1d1
waldenhillegass <waldenhillegass@gmail.com>
2022-10-26 13:40:20 -0700

    sd

--- radiotesting/version.py
+++ radiotesting/version.py
@@ -5,7 +5,7 @@ import serial
 def main():
    ser = serial.Serial('/dev/tty0', baudrate=9600, timeout=1, stopbits=serial.STOPBITS_ONE)
    ser.write(0xC3C3C3)
-   print(hex(ser.read(8)))
+   print(ser.read(8))
 
 if __name__ == "__main__":
    main()
